041671c27bed8fd1adc34c5a68b920eb90ba9511
William Guimont-Martin <william.guimont-martin.1@ulaval.ca>
2020-10-21 17:24:01 -0400

    9

---
+++ 009.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(require racket/stream)
+
+(define (triplet? a b c)
+  (=
+   (+ (expt a 2) (expt b 2))
+   (expt c 2)))
+
+(define (sum? a b c)
+  (= 100 (+ a b c)))
+
+(define triplet '(0 0 0))
+(for ([i (range 1 1000)])
+  (for ([j (range i 1000)])
+    (let ([k (- 1000 (+ i j))])
+      (when (and (triplet? i j k))
+        (set! triplet `(,i ,j ,k))))))
+
+(foldl * 1 triplet)
+
+
+
