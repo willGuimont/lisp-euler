e3c9e502d81b24109d9ebdc7fde964ded8174d05
William Guimont-Martin <wil9876543210@hotmail.com>
2020-11-08 15:41:35 -0500

    format 18

--- 018.rkt
+++ 018.rkt
@@ -10,13 +10,13 @@
     53 67 30 73 16 69 87 40 31 04 62 98 27 23 09 70
     98 73 93 38 53 60 04 23))
 
+
 (define (triangle)
   (define (iter n xs c)
     (cond [(= 0 (length xs)) c]
           [else
            (iter (add1 n) (drop xs n) (vector-append c (vector (apply vector (take xs n)))))]))
   (iter 1 numbers (list->vector '())))
-
 
 
 (define (solve world)
